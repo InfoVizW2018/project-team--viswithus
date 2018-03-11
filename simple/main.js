92b94f69c00f85405f1030d8d5c871983e159b85
jbelford <jackbelford12@live.ca>
2018-03-11 16:09:55 -0700

    basic hive

--- simple/main.js
+++ simple/main.js
@@ -1,3 +1,4 @@
+
 let people, performanceTotals, playSales, playTotals, plays
 
 d3.queue()
@@ -18,7 +19,8 @@ function loadHandler(err, ppl, pttls, psls, plttls, pl) {
   playTotals = plttls;
   plays = pl;
 
-  initStatistics();  
+  initStatistics();
+  renderHivePlot();
   $('.loader').toggleClass(['active', 'disabled']);
 }
 
@@ -30,6 +32,72 @@ function initStatistics() {
     return $('<div></div>').addClass('statistic').append(valDiv, labelDiv);
   });
   $('.statistics').append(newStats)
+}
+
+function renderHivePlot() {
+  var width = 960;
+  var height = 500;
+  var innerRadius = 40;
+  var outerRadius = 240;
+
+  var angle = d3.scalePoint().domain(d3.range(4)).range([0, 2 * Math.PI]);
+  var radius = d3.scaleLinear().range([innerRadius, outerRadius]);
+  var color = d3.scaleOrdinal(d3.schemeCategory10).domain(d3.range(20));
+
+  var nodes = [
+    {x: 0, y: .1},
+    {x: 0, y: .9},
+    {x: 1, y: .2},
+    {x: 1, y: .3},
+    {x: 2, y: .1},
+    {x: 2, y: .8}
+  ];
+  
+  var links = [
+    {source: nodes[0], target: nodes[2]},
+    {source: nodes[1], target: nodes[3]},
+    {source: nodes[2], target: nodes[4]},
+    {source: nodes[2], target: nodes[5]},
+    {source: nodes[3], target: nodes[5]},
+    {source: nodes[4], target: nodes[0]},
+    {source: nodes[5], target: nodes[1]}
+  ];
+  
+  var svg = d3.select("#graph").append("svg")
+      .attr("width", width)
+      .attr("height", height)
+    .append("g")
+      .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+  
+  svg.selectAll(".axis")
+      .data(d3.range(3))
+    .enter().append("line")
+      .attr("class", "axis")
+      .attr("transform", function(d) { return "rotate(" + degrees(angle(d)) + ")"; })
+      .attr("x1", radius.range()[0])
+      .attr("x2", radius.range()[1]);
+  
+  svg.selectAll(".link")
+      .data(links)
+    .enter().append("path")
+      .attr("class", "link")
+      .attr("d", d3.hive.link()
+      .angle(function(d) { return angle(d.x); })
+      .radius(function(d) { return radius(d.y); }))
+      .style("stroke", function(d) { return color(d.source.x); });
+  
+  svg.selectAll(".node")
+      .data(nodes)
+    .enter().append("circle")
+      .attr("class", "node")
+      .attr("transform", function(d) { return "rotate(" + degrees(angle(d.x)) + ")"; })
+      .attr("cx", function(d) { return radius(d.y); })
+      .attr("r", 5)
+      .style("fill", function(d) { return color(d.x); });
+}
+
+function degrees(radians) {
+  return radians / Math.PI * 180 - 90;
 }
 
 function showModal() {

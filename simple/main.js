b34a0579e27a89a13a7f1647fb4e989abf7952fe
Jack Belford <jackbelford12@live.ca>
2018-03-29 15:40:23 -0700

    link mouseover

--- simple/main.js
+++ simple/main.js
@@ -154,7 +154,7 @@ function initNodesAndLinks() {
       if (i > -1) {
         genreNodes[i].linked++;
         play.linked++;
-        links.push({ source: play, target: genreNodes[i] });
+        links.push({ source: genreNodes[i], target: play });
       }
     }
   });
@@ -344,6 +344,8 @@ function degrees(radians) {
 function linkMouseover(d) {
   svg.selectAll(".link").classed("active", function(p) { return p === d; });
   svg.selectAll(".node").classed("active", function(p) { return p === d.source || p === d.target; });
+  d.source.popupFunc();
+  d.target.popupFunc();
 }
 
 // Highlight the node and connected links on mouseover.
@@ -360,113 +362,113 @@ function mouseout() {
 }
 
 function renderAuthorGenreDistDonutChart(author){
-      var playsByCurrAuthor = plays.filter( function (play) {
-        return play.author == author;
-      });
+  var playsByCurrAuthor = plays.filter( function (play) {
+    return play.author == author;
+  });
 
-      var numPlays = playsByCurrAuthor.length;
+  var numPlays = playsByCurrAuthor.length;
 
-      var genresList = [];
+  var genresList = [];
 
-      playsByCurrAuthor.forEach(function(play){
-        genresList.push(play.genre);
-      });
+  playsByCurrAuthor.forEach(function(play){
+    genresList.push(play.genre);
+  });
 
-      const counts = genresList.reduce( (tally, genre) => {
-        tally[genre] = (tally[genre] || 0) + 1 ;
-        return tally;
-      } , {});
+  const counts = genresList.reduce( (tally, genre) => {
+    tally[genre] = (tally[genre] || 0) + 1 ;
+    return tally;
+  } , {});
 
-      var seedData = [];
-      var len = counts.length;
+  var seedData = [];
+  var len = counts.length;
 
-      for (var genre in counts)
-          seedData.push({ "genre": genre, "value": counts[genre]});
+  for (var genre in counts)
+      seedData.push({ "genre": genre, "value": counts[genre]});
 
-      // Define size & radius of donut pie chart
-      var width = 450,
-          height = 450,
-          radius = Math.min(width, height) / 2;
+  // Define size & radius of donut pie chart
+  var width = 450,
+      height = 450,
+      radius = Math.min(width, height) / 2;
 
-      // Define arc colours
-      var colour = d3.scaleOrdinal(d3.schemeCategory10);
+  // Define arc colours
+  var colour = d3.scaleOrdinal(d3.schemeCategory10);
 
-      // Define arc ranges
-      var arcText = d3.scaleOrdinal()
-        .range([0, width]);
+  // Define arc ranges
+  var arcText = d3.scaleOrdinal()
+    .range([0, width]);
 
-      // Determine size of arcs
-      var arc = d3.arc()
-        .innerRadius(radius - 130)
-        .outerRadius(radius - 10);
+  // Determine size of arcs
+  var arc = d3.arc()
+    .innerRadius(radius - 130)
+    .outerRadius(radius - 10);
 
-      // Create the donut pie chart layout
-      var pie = d3.pie()
-        .value(function (d) { return d["value"]; })
-        .sort(null);
+  // Create the donut pie chart layout
+  var pie = d3.pie()
+    .value(function (d) { return d["value"]; })
+    .sort(null);
 
-      d3.select("#author-donut-chart").selectAll("*").remove();
+  d3.select("#author-donut-chart").selectAll("*").remove();
 
-      // Append SVG attributes and append g to the SVG
-      var svg = d3.select("#author-donut-chart")
-        .attr("width", width)
-        .attr("height", height)
-        .append("g")
-          .attr("transform", "translate(" + radius + "," + radius + ")");
+  // Append SVG attributes and append g to the SVG
+  var svg = d3.select("#author-donut-chart")
+    .attr("width", width)
+    .attr("height", height)
+    .append("g")
+      .attr("transform", "translate(" + radius + "," + radius + ")");
 
-      // Define inner circle
-      svg.append("circle")
-        .attr("cx", 0)
-        .attr("cy", 0)
-        .attr("r", 100)
-        .attr("fill", "#fff") ;
+  // Define inner circle
+  svg.append("circle")
+    .attr("cx", 0)
+    .attr("cy", 0)
+    .attr("r", 100)
+    .attr("fill", "#fff") ;
 
-      // Calculate SVG paths and fill in the colours
-      var g = svg.selectAll(".arc")
-        .data(pie(seedData))
-        .enter().append("g")
-        .attr("class", "arc")
+  // Calculate SVG paths and fill in the colours
+  var g = svg.selectAll(".arc")
+    .data(pie(seedData))
+    .enter().append("g")
+    .attr("class", "arc")
 
-        // Make each arc clickable
-        // .on("click", function(d, i) {
-        //   window.location = seedData[i].link;
-        // });
+    // Make each arc clickable
+    // .on("click", function(d, i) {
+    //   window.location = seedData[i].link;
+    // });
 
-    	// Append the path to each g
-    	g.append("path")
-      	.attr("d", arc)
-      	.attr("fill", function(d, i) {
-        	return colour(i);
-      	});
+  // Append the path to each g
+  g.append("path")
+    .attr("d", arc)
+    .attr("fill", function(d, i) {
+      return colour(i);
+    });
 
-    	// Append text labels to each arc
-    	g.append("text")
-      	.attr("transform", function(d) {
-        	return "translate(" + arc.centroid(d) + ")";
-      	})
-      	.attr("dy", ".35em")
-      	.style("text-anchor", "middle")
-      	.attr("fill", "#fff")
-        .attr("background-color", "#000")
-    		.text(function(d,i) { return seedData[i].genre; })
+  // Append text labels to each arc
+  g.append("text")
+    .attr("transform", function(d) {
+      return "translate(" + arc.centroid(d) + ")";
+    })
+    .attr("dy", ".35em")
+    .style("text-anchor", "middle")
+    .attr("fill", "#fff")
+    .attr("background-color", "#000")
+    .text(function(d,i) { return seedData[i].genre; })
 
-      g.selectAll(".arc text").call(wrap, arcText.range([0, width]));
+  g.selectAll(".arc text").call(wrap, arcText.range([0, width]));
 
-      // Append text to the inner circle
-      svg.append("text")
-        .attr("dy", "-0.5em")
-        .style("text-anchor", "middle")
-        .attr("class", "inner-circle")
-        .attr("fill", "#36454f")
-        .text(function(d) { return 'Plays Written'; });
+  // Append text to the inner circle
+  svg.append("text")
+    .attr("dy", "-0.5em")
+    .style("text-anchor", "middle")
+    .attr("class", "inner-circle")
+    .attr("fill", "#36454f")
+    .text(function(d) { return 'Plays Written'; });
 
-      svg.append("text")
-        .attr("dy", "1.0em")
-        .attr("size", "12")
-        .style("text-anchor", "middle")
-        .attr("class", "inner-circle donut-label")
-        .attr("fill", "#36454f")
-        .text(function(d) { return numPlays; });
+  svg.append("text")
+    .attr("dy", "1.0em")
+    .attr("size", "12")
+    .style("text-anchor", "middle")
+    .attr("class", "inner-circle donut-label")
+    .attr("fill", "#36454f")
+    .text(function(d) { return numPlays; });
 
 }
 
